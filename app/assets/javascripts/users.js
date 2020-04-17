8fb9e6c13279f608ac8fac9ac7f0c9d84e72cf2b
ShimizuKosuke19910320 <shimizu.kosuke.1031@gmail.com>
2020-04-17 21:39:03 +0900

    イベント時に非同期通信を可能にするための実装

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,7 +2,20 @@ $(function(){
 
   $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
-    console.log(input);
-  });
+
+    $.ajax({
+      url: "/users",
+      type: "GET",
+      data: { keyword: input },
+      dataType: "json"
+    })
+      .done(function(users) {
+        console.log("success");
+      })
+      .fail(function() {
+        console.log("failure");
+      });
+
+    });
 
 });
